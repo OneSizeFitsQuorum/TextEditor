6f86a57642f400621fe16e91600cb84ea2c756a6
LebronAl <1025599465@qq.com>
2018-10-24 10:41:14 +0800

    txy job

--- textEditer.asm
+++ textEditer.asm
@@ -11,16 +11,22 @@ include         user32.inc
 includelib      user32.lib
 include         kernel32.inc
 includelib      kernel32.lib
+include         comctl32.inc
+includelib      comctl32.lib
+include         comdlg32.inc
+includelib      comdlg32.lib
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ;Equ 等值定义
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+;Menu
 IDM_MAIN         equ           1000h
 
 IDM_NEW          equ           1101h
 IDM_OPEN         equ           1102h
 IDM_SAVE          equ           1103h
 IDM_SAVEAS      equ           1104h
-IDM_QUIT          equ           1105h
+IDM_PAGESET     equ          1105h
+IDM_QUIT          equ           1106h
 
 IDM_UNDO        equ           1201h
 IDM_CUT            equ           1202h
@@ -38,21 +44,72 @@ IDM_FONT         equ             1301h
 
 IDM_HELP          equ             1401h
 IDM_ABOUT       equ             1402h
+
+;Accelerators 	
+IDA_MAIN	      equ           2000h
+;Statusbar
+IDCC_STATUSBAR    equ      3000h
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ;数据段
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  
 .data?
 	hInstance dd ?
 	hWinMain dd ?
-	hMenu dd ?
+	hStatusBar dd ?
+	hWinEdit dd ?
+	hMainMenu dd ?
 	hSubMenu dd ?
+
 .const
 	szClassName db 'MyTextEditer',0
 	szCaptionMain db 'TextEditer++',0
 	szText db "Let's do something!",0
+
+	szDllRiched20    db       'riched20.dll',0
+	szClassEdit         db       'RichEdit20A',0
+	szFont               db        '宋体',0
+	szTxt                 db         '无格式文本',0
+
+	dwStatusWidth dd          200,500,300,-1
+
+	szAboutTitle     db          '关于TextEditer++的信息',0
+	szAbout            db         '基于Win32的文本编辑器',0dh,0ah,0dh,\
+										  '开发者：谭新宇 卢北辰 刘文华',0dh,0ah,0
+
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ;代码段
 .code
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+_Init PROC
+	local @stCharFormat: CHARFORMAT
+	
+	invoke CreateWindowEx,WS_EX_OVERLAPPEDWINDOW,OFFSET szClassEdit,NULL,\
+	WS_CHILD or WS_VISIBLE or WS_VSCROLL or ES_AUTOVSCROLL or ES_MULTILINE or ES_NOHIDESEL or ES_WANTRETURN or ES_LEFT,\
+	0,0,0,0,hWinMain,NULL,hInstance,NULL
+	mov     hWinEdit, eax
+	invoke  SendMessage, hWinEdit, EM_SETTEXTMODE, TM_PLAINTEXT, 0
+	invoke  SendMessage, hWinEdit, EM_SETEVENTMASK, 0, ENM_MOUSEEVENTS
+    invoke  SendMessage, hWinEdit, EM_SETMARGINS, EC_RIGHTMARGIN or EC_LEFTMARGIN, 00050000h     ;加行号的时候得改
+	invoke  RtlZeroMemory, addr @stCharFormat, sizeof @stCharFormat  
+    invoke  SendMessage, hWinEdit, EM_EXLIMITTEXT, NULL, -1
+    mov     @stCharFormat.cbSize, sizeof @stCharFormat
+    mov     @stCharFormat.dwMask, CFM_BOLD or CFM_COLOR or CFM_FACE or CFM_ITALIC or CFM_SIZE or CFM_UNDERLINE or CFM_STRIKEOUT
+    mov     @stCharFormat.yHeight, 12 * 20
+	invoke  SendMessage, hWinEdit, EM_SETCHARFORMAT, SCF_ALL, addr @stCharFormat
+    invoke  lstrcpy, addr @stCharFormat.szFaceName, addr szFont
+
+	ret
+_Init ENDP
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+_PageSet PROC
+	local @stPs:PAGESETUPDLG
+	invoke RtlZeroMemory,addr @stPs,sizeof @stPs
+	mov @stPs.lStructSize,sizeof @stPs
+	push hWinMain
+	pop @stPs.hwndOwner
+	invoke PageSetupDlg,addr @stPs
+	ret
+_PageSet ENDP
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 _Quit  PROC
 				invoke DestroyWindow, hWinMain
@@ -62,26 +119,72 @@ _Quit  ENDP
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ;窗口过程
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-_ProcWinMain PROC USES ebx edi esi,hWnd,uMsg,wParam,lParam
-	local @stPs:PAINTSTRUCT
+_ProcWinMain PROC USES ebx edi esi,hWnd,uMsg,wParam,lParam 
+    ;wParam参数高16位是通知码，低16位是命令ID  lParam是发送WM_COMMAND消息的子窗口句柄
+    ;菜单消息的通知码是0，加速键消息的通知码是1
+    ;对于菜单和加速键引发的WM_COMMAND消息，lParam的值为0
 	local @stRect:RECT
-	local @hDc
+	local @stPos: POINT
 	mov eax,uMsg
 ;**********************************************************************************
-	.if			eax == WM_CREATE
-				invoke GetSubMenu,hMenu,1
+	.if	eax == WM_CREATE
+				push hWnd
+				pop hWinMain
+				call _Init
+				invoke GetSubMenu,hMainMenu,1
 				mov hSubMenu,eax
-
 ;**********************************************************************************
-	.elseif	eax == WM_PAINT
-				invoke BeginPaint, hWnd, addr @stPs
-				mov @hDc, eax
-				invoke GetClientRect, hWnd, addr @stRect
-				invoke DrawText,@hDc, addr szText,-1,\
-				addr @stRect,\
-				DT_SINGLELINE Or DT_CENTER Or DT_VCENTER
-				invoke EndPaint, hWnd, addr @stPs
+	.elseif   eax == WM_COMMAND
+				mov eax,wParam
+				.if ax == IDM_NEW
 
+				.elseif ax == IDM_OPEN
+
+				.elseif ax == IDM_SAVE
+
+				.elseif ax == IDM_SAVEAS
+
+				.elseif ax == IDM_PAGESET
+					call _PageSet
+				.elseif ax == IDM_QUIT
+					call _Quit
+				.elseif ax == IDM_UNDO 
+
+				.elseif ax == IDM_PASTE  
+
+				.elseif ax == IDM_DELETE 
+
+				.elseif ax == IDM_FIND
+
+				.elseif ax == IDM_FINDNEXT
+
+				.elseif ax == IDM_REPLACE 
+
+				.elseif ax == IDM_TURN
+
+				.elseif ax == IDM_ALL 
+
+				.elseif ax == IDM_DATE
+
+				.elseif ax == IDM_FONT
+
+				.elseif ax == IDM_HELP
+
+				.elseif ax == IDM_ABOUT
+						invoke  MessageBox, hWinMain,addr szAbout, addr szAboutTitle, MB_OK
+				.endif		
+;**********************************************************************************
+	.elseif	eax == WM_RBUTTONDOWN
+				invoke  GetCursorPos, addr @stPos
+				invoke  TrackPopupMenu, hSubMenu, TPM_LEFTALIGN, @stPos.x, @stPos.y, NULL, hWinEdit, NULL
+;**********************************************************************************
+    .elseif    eax == WM_SIZE
+	            invoke  MoveWindow, hStatusBar, 0, 0, 0, 0, TRUE
+                invoke  GetClientRect, hWnd, addr @stRect
+                mov     ebx, @stRect.bottom
+
+                sub      ebx, 0018h
+                invoke  MoveWindow, hWinEdit, 0, 0, @stRect.right, ebx, TRUE
 ;**********************************************************************************
 	.elseif	eax == WM_CLOSE
 				call _Quit
@@ -98,12 +201,26 @@ _ProcWinMain ENDP
 _WinMain	PROC
 	local @stWndClass:WNDCLASSEX
 	local @stMsg:MSG
+	local @hAccelerator: DWORD
+	local @hRichEdit: DWORD
+;**********************************************************************************
+;注册文本编辑框
+;**********************************************************************************
+	invoke LoadLibrary,addr szDllRiched20
+	mov @hRichEdit,eax
+;**********************************************************************************
+;得到窗口句柄并载入菜单和加速键
+;**********************************************************************************
 	invoke GetModuleHandle,NULL
 	mov hInstance,eax
-	invoke RtlZeroMemory,addr @stWndClass,sizeof @stWndClass
+	invoke LoadMenu,hInstance,IDM_MAIN
+	mov hMainMenu,eax
+	invoke LoadAccelerators,hInstance,IDA_MAIN
+	mov @hAccelerator,eax
 ;**********************************************************************************
 ;注册窗口类
 ;**********************************************************************************
+	invoke RtlZeroMemory,addr @stWndClass,sizeof @stWndClass
 	invoke LoadCursor,0, IDC_ARROW
 	mov @stWndClass.hCursor, eax
 	invoke LoadIcon,0,IDI_APPLICATION
@@ -121,10 +238,19 @@ _WinMain	PROC
 ;**********************************************************************************
 	invoke CreateWindowEx,WS_EX_CLIENTEDGE,\
 				offset szClassName,offset szCaptionMain,\
-				WS_OVERLAPPEDWINDOW or WS_VSCROLL ,\
+				WS_OVERLAPPEDWINDOW ,\
 				100,100,600,400,\
-				NULL,NULL,hInstance,NULL
+				NULL,hMainMenu,hInstance,NULL
 	mov hWinMain,eax
+;**********************************************************************************
+;创建状态栏
+;**********************************************************************************
+	invoke CreateStatusWindow,WS_CHILD or WS_VISIBLE or SBARS_SIZEGRIP,NULL,hWinMain,IDCC_STATUSBAR
+	mov hStatusBar,eax
+	invoke  SendMessage, hStatusBar, SB_SETPARTS, 4, offset dwStatusWidth
+;**********************************************************************************
+;显示并更新窗口
+;**********************************************************************************
 	invoke ShowWindow,hWinMain,SW_SHOWNORMAL
 	invoke UpdateWindow,hWinMain
 ;**********************************************************************************
@@ -133,9 +259,13 @@ _WinMain	PROC
 	.while TRUE
 				invoke GetMessage, addr @stMsg, NULL,0,0
 				.break .if eax==0
-				invoke TranslateMessage, addr @stMsg
-				invoke DispatchMessage, addr @stMsg
+				invoke TranslateAccelerator,hWinMain,@hAccelerator,addr @stMsg
+				.if eax == 0
+					invoke TranslateMessage, addr @stMsg
+					invoke DispatchMessage, addr @stMsg
+				.endif
 	.endw
+	invoke  FreeLibrary, @hRichEdit
 	ret
 _WinMain ENDP
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
